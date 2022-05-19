0991ac625660cca70cef7f62ba7b75b29bbd2033
MashaNes <masa_nesic@outlook.com>
2022-05-19 13:57:37 +0200

    run_optimizations adapted

--- code/src/optimizations/run_optimisations.c
+++ code/src/optimizations/run_optimisations.c
@@ -1,4 +1,6 @@
+#include <optimizations/optimizations_0.h>
 #include <optimizations/optimizations_1.h>
+#include <optimizations/optimizations_2.h>
 #include <optimizations/alg_opt_1.h>
 #include <optimizations/alg_opt_2.h>
 #include <asl.h>
@@ -79,9 +81,11 @@ int main(int argc, char const* argv[]) {
 
         // add here the optimisation descriptions
         printf("List of optimisations available:\n");
+        printf("\t0 - Optimisation 0\n");
         printf("\t1 - Optimisation 1\n");
         printf("\t2 - Algoritmh opt 1\n");
         printf("\t3 - Algoritmh opt 2\n");
+        printf("\t4 - Optimization 2\n");
         return -1;
     }
     FILE* fp = NULL;
@@ -123,6 +127,9 @@ int main(int argc, char const* argv[]) {
 
     switch (opt_num)
     {
+    case 0:
+        run_factorization = &nnm_factorization_opt0;
+        break;
     case 1:
         run_factorization = &nnm_factorization_opt1;
         break;
@@ -131,6 +138,9 @@ int main(int argc, char const* argv[]) {
         break;
     case 3:
         run_factorization = &nnm_factorization_aopt2;
+        break;
+    case 4:
+        run_factorization = &nnm_factorization_opt2;
         break;
     default:
         printf("Invalid opt number. Quitting\n");
