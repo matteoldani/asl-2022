73731f8acfe1dde09156bb97dad6580cc16dd5cc
vgsteiger <vgsteiger@student.ethz.ch>
2022-05-22 11:05:01 +0200

    Unrolled W iteration

--- code/src/optimizations/optimization_4.c
+++ code/src/optimizations/optimization_4.c
@@ -12,27 +12,24 @@ typedef unsigned long long myInt64;
 
 static unsigned int double_size = sizeof(double);
 
-static void transpose(double *src, double *dst,  const int N, const int M) {
+static void transpose(double *src, double *dst, const int N, const int M) {
 
     //NEW - introduced blocking and simlified index calcs (code motion, strength reduction)
     int nB = BLOCK_SIZE_TRANS;
     int src_i = 0, src_ii;
 
     //NEW - introduced double loop to avoid calculating DIV and MOD M*N times
-    for(int i = 0; i < N; i += nB)
-    {
-        for(int j = 0; j < M; j += nB)
-        {
+    for (int i = 0; i < N; i += nB) {
+        for (int j = 0; j < M; j += nB) {
             src_ii = src_i;
-            for(int ii = i; ii < i + nB; ii++)
-            {
-                for(int jj = j; jj < j + nB; jj++)
-                    dst[N*jj + ii] = src[src_ii + jj];
+            for (int ii = i; ii < i + nB; ii++) {
+                for (int jj = j; jj < j + nB; jj++)
+                    dst[N * jj + ii] = src[src_ii + jj];
                 src_ii += M;
             }
         }
         src_i += nB * M;
-    }   
+    }
 }
 
 /**
@@ -47,11 +44,12 @@ static void transpose(double *src, double *dst,  const int N, const int M) {
  * @param R_n_row   is the number of rows in the result
  * @param R_n_col   is the number of columns in the result
  */
-void matrix_mul_opt4(double *A, int A_n_row, int A_n_col, double*B, int B_n_row, int B_n_col, double*R, int R_n_row, int R_n_col) {
+void matrix_mul_opt4(double *A, int A_n_row, int A_n_col, double *B, int B_n_row, int B_n_col, double *R, int R_n_row,
+                     int R_n_col) {
 
     //NOTE - we need a row of A, whole block of B and 1 element of R in the cache (normalized for the cache line)
     //NOTE - when taking LRU into account, that is 2 rows of A, the whole block of B and 1 row + 1 element of R
-    
+
     int Rij = 0, Ri = 0, Ai = 0, Aii, Rii;
     int nB = BLOCK_SIZE_MMUL;
 
@@ -59,9 +57,9 @@ void matrix_mul_opt4(double *A, int A_n_row, int A_n_col, double*B, int B_n_row,
 
     memset(R, 0, double_size * R_n_row * R_n_col);
 
-    for (int i = 0; i < A_n_row; i+=nB) {
-        for (int j = 0; j < B_n_col; j+=nB) {
-            for (int k = 0; k < A_n_col; k+=nB) {
+    for (int i = 0; i < A_n_row; i += nB) {
+        for (int j = 0; j < B_n_col; j += nB) {
+            for (int k = 0; k < A_n_col; k += nB) {
                 Rii = Ri;
                 Aii = Ai;
                 for (int ii = i; ii < i + nB; ii++) {
@@ -94,8 +92,10 @@ void matrix_mul_opt4(double *A, int A_n_row, int A_n_col, double*B, int B_n_row,
  * @param R_n_row   is the number of rows in the result
  * @param R_n_col   is the number of columns in the result
  */
-void matrix_rtrans_mul_opt4(double* A, int A_n_row, int A_n_col, double* B, int B_n_row, int B_n_col, double* R, int R_n_row, int R_n_col) {
-    
+void
+matrix_rtrans_mul_opt4(double *A, int A_n_row, int A_n_col, double *B, int B_n_row, int B_n_col, double *R, int R_n_row,
+                       int R_n_col) {
+
     int Rij = 0, Ri = 0, Ai = 0, Bj, Rii, Aii, Bjj;
     int nB = BLOCK_SIZE_RTRANSMUL;
 
@@ -103,10 +103,10 @@ void matrix_rtrans_mul_opt4(double* A, int A_n_row, int A_n_col, double* B, int 
 
     memset(R, 0, double_size * R_n_row * R_n_col);
 
-    for (int i = 0; i < A_n_row; i+=nB) {
+    for (int i = 0; i < A_n_row; i += nB) {
         Bj = 0;
-        for (int j = 0; j < B_n_row; j+=nB) {
-            for (int k = 0; k < A_n_col; k+=nB){
+        for (int j = 0; j < B_n_row; j += nB) {
+            for (int k = 0; k < A_n_col; k += nB) {
                 Aii = Ai;
                 Rii = Ri;
                 for (int ii = i; ii < i + nB; ii++) {
@@ -145,15 +145,14 @@ void matrix_rtrans_mul_opt4(double* A, int A_n_row, int A_n_col, double* B, int 
  * @param norm_V    is 1 / the norm of matrix V
  * @return          is the error
  */
-inline double error(double* approx, double* V, double* W, double* H, int m, int n, int r, int mn, double norm_V) {
+inline double error(double *approx, double *V, double *W, double *H, int m, int n, int r, int mn, double norm_V) {
 
     matrix_mul_opt4(W, m, r, H, r, n, approx, m, n);
 
     double norm_approx, temp;
 
     norm_approx = 0;
-    for (int i = 0; i < mn; i++)
-    {
+    for (int i = 0; i < mn; i++) {
         temp = V[i] - approx[i];
         norm_approx += temp * temp;
     }
@@ -175,7 +174,8 @@ inline double error(double* approx, double* V, double* W, double* H, int m, int 
  * @param maxIteration  maximum number of iterations that can run
  * @param epsilon       difference between V and W*H that is considered acceptable
  */
-double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, int r, int maxIteration, double epsilon) {
+double
+nnm_factorization_opt4(double *V_rowM, double *W, double *H, int m, int n, int r, int maxIteration, double epsilon) {
 
     double *Wt, *Wtmp, *Ht, *Htmp, *tmp;
     double *V_colM;
@@ -191,12 +191,12 @@ double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, 
     V_colM = malloc(double_size * mn);
 
     int nB = BLOCK_SIZE_NNMF;
-  
+
     // this is required to be done here to reuse the same run_opt.
     // does not change the number of flops
-    for (int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-           V_colM[j*m + i] = V_rowM[i*n + j];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            V_colM[j * m + i] = V_rowM[i * n + j];
         }
     }
 
@@ -212,7 +212,7 @@ double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, 
     denominator_W = malloc(double_size * mr);
     denominator_l_W = malloc(double_size * mn);
 
-    double* approximation; //m x n
+    double *approximation; //m x n
     approximation = malloc(double_size * mn);
 
     double norm_V = 0;
@@ -233,13 +233,13 @@ double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, 
             nij = i * n + j;
             dij = i * r + j;
             numerator[nij] = 0;
-            if(j<r){
+            if (j < r) {
                 denominator_l[dij] = 0;
             }
-            for (int k = 0; k < m; k++){
+            for (int k = 0; k < m; k++) {
                 wt = Wt[i * m + k];
                 numerator[nij] += wt * V_colM[j * m + k];
-                if(j<r){
+                if (j < r) {
                     denominator_l[dij] += wt * Wt[j * m + k];
                 }
             }
@@ -254,7 +254,7 @@ double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, 
         for (int j = 0; j < n; j++) {
             Rij = i * n + j;
             denominator[Rij] = 0;
-            for (int k = 0; k < r; k++){
+            for (int k = 0; k < r; k++) {
                 denominator[Rij] += denominator_l[i * r + k] * Ht[j * r + k];
             }
             Htmp[Rij] = H[Rij] * numerator[Rij] / denominator[Rij];
@@ -272,28 +272,38 @@ double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, 
             for (int j = 0; j < r; j++) {
                 Rij = i * r + j;
                 numerator_W[Rij] = 0;
-                if(i < r){
+                if (i < r) {
                     denominator_l[Rij] = 0;
                 }
-                for (int k = 0; k < n; k++){
+                for (int k = 0; k < n; k++) {
                     h = H[j * n + k];
                     numerator_W[Rij] += V_rowM[i * n + k] * h;
-                    if(i < r){
+                    if (i < r) {
                         denominator_l[Rij] += H[i * n + k] * h;
                     }
                 }
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < r; j++) {
-                Rij = i * r + j;
-                denominator_W[Rij] = 0;
-                for (int k = 0; k < r; k++) {
-                    denominator_W[Rij] += W[i * r + k] * denominator_l[k * r + j];
+        int Rij = 0, Ri = 0, Rii;
+        for (int i = 0; i < m; i += nB) {
+            for (int j = 0; j < r; j + nB) {
+                for (int k = 0; k < r; k += nB) {
+                    Rii = Ri;
+                    for (int ii = i; ii < i + nB; ii++) {
+                        for (int jj = j; jj < j + nB; jj++) {
+                            Rij = Rii + jj;
+                            for (int kk = k; kk < k + nB; kk++)
+                                denominator_W[Rij] += W[Rii + kk] * denominator_l[kk * r + jj];
+                            Wtmp[Rij] = W[Rij] * numerator_W[Rij] / denominator_W[Rij];
+                            // TODO: At this point interleave with the next iteration of H
+                        }
+                        Rii += r;
+                    }
                 }
-                Wtmp[Rij] = W[Rij] * numerator_W[Rij] / denominator_W[Rij];
             }
+            Ri += nB * r;
+            Ai += nB * r;
         }
 
         tmp = Wtmp;
@@ -317,13 +327,13 @@ double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, 
                 nij = i * n + j;
                 dij = i * r + j;
                 numerator[nij] = 0;
-                if(j<r){
+                if (j < r) {
                     denominator_l[dij] = 0;
                 }
-                for (int k = 0; k < m; k++){
+                for (int k = 0; k < m; k++) {
                     wt = Wt[i * m + k];
                     numerator[nij] += wt * V_colM[j * m + k];
-                    if(j<r){
+                    if (j < r) {
                         denominator_l[dij] += wt * Wt[j * m + k];
                     }
                 }
@@ -338,7 +348,7 @@ double nnm_factorization_opt4(double *V_rowM, double*W, double*H, int m, int n, 
             for (int j = 0; j < n; j++) {
                 Rij = i * n + j;
                 denominator[Rij] = 0;
-                for (int k = 0; k < r; k++){
+                for (int k = 0; k < r; k++) {
                     denominator[Rij] += denominator_l[i * r + k] * Ht[j * r + k];
                 }
                 Htmp[Rij] = H[Rij] * numerator[Rij] / denominator[Rij];
