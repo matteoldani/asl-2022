42daf8d15a40e820776d79b7fc8ac065b6a3ae58
vgsteiger <vgsteiger@student.ethz.ch>
2022-05-29 11:19:55 +0200

    Extended runs

--- docs/plotting/roofline/roofline.py
+++ docs/plotting/roofline/roofline.py
@@ -2,14 +2,14 @@ import os
 import time 
 
 labels = ['bs1', 'bs2', 'opt0', 'opt1', 'aopt1', 'aopt2', 'opt2', 'opt3', 'opt21', 'opt22', 'opt23', 'opt24', 'opt31', 'opt32', 'opt33', 'opt34']
-sizes = [240, 320, 400, 480, 560, 640] # 80, 160, 
+sizes = [200, 400, 800, 1600, 2400, 2960, 3200]
 
 
 for size in sizes:
     for label, i in zip(labels,range(len(labels))):
         index = i + 1
-        os.system(f'advixe-cl -collect survey -project-dir /home/asl/asl-2022-vgsteiger/code/advi_results -- /home/asl/asl-2022-vgsteiger/code/build/roofline {index} {size} 10')
-        os.system(f'advixe-cl -collect tripcounts -flop -stacks -project-dir /home/asl/asl-2022-vgsteiger/code/advi_results -- /home/asl/asl-2022-vgsteiger/code/build/roofline {index} {size} 10')
+        os.system(f'advixe-cl -collect survey -project-dir /home/asl/asl-2022-vgsteiger/code/advi_results -- /home/asl/asl-2022-vgsteiger/code/build/roofline {index} {size} 1')
+        os.system(f'advixe-cl -collect tripcounts -flop -stacks -project-dir /home/asl/asl-2022-vgsteiger/code/advi_results -- /home/asl/asl-2022-vgsteiger/code/build/roofline {index} {size} 1')
         os.system(f'advixe-cl -report roofline -project-dir /home/asl/asl-2022-vgsteiger/code/advi_results -report-output /home/asl/asl-2022-vgsteiger/docs/outputs/roofline/advisor-roofline-size-{size}-{label}.html')
 
         time.sleep(10)
